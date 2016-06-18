f3a1d37356f08bfe69b911bca697cb5dc3d587a2
Richard Hibbitts <rhibbitts@technopoetic.com>
2016-06-18 09:59:23 -0400

    Part 5.  User login

--- app/models.py
+++ app/models.py
@@ -6,9 +6,27 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
-
+    
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
